a4802663c15b0065d57d57a2fd25cdfa63cc6b2b
vivcogit <vivcogit@gmail.com>
2024-09-21 23:19:14 +0200

    decompose

--- main.go
+++ main.go
@@ -2,12 +2,39 @@ package main
 
 import (
 	"bytes"
+	"errors"
 	"image/color"
 	"log"
 
 	"github.com/disintegration/imaging"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
+
+func handleGradient(bot *tgbotapi.BotAPI, userId, chatId int64) error {
+	userAvatar, err := GetUserAvatar(bot, userId)
+	if err != nil {
+		log.Printf("[ERR] error geting user avatar: %v", err)
+		return err
+	}
+
+	gradient := CreateGradient(userAvatar.Bounds().Dx(), userAvatar.Bounds().Dy(), color.NRGBA{R: 0, G: 0, B: 255, A: 255}, color.NRGBA{R: 255, G: 0, B: 255, A: 255})
+
+	imgWithGradient := OverlayImage(userAvatar, gradient, 0.5)
+
+	var buf bytes.Buffer
+	err = imaging.Encode(&buf, imgWithGradient, imaging.PNG)
+	if err != nil {
+		log.Printf("[ERR] error saving imgWithGradient: %v", err)
+		return errors.New("error due overlaying")
+	}
+
+	photo := tgbotapi.NewPhoto(chatId, tgbotapi.FileBytes{
+		Name:  "avatar_with_gradient.png",
+		Bytes: buf.Bytes(),
+	})
+	_, err = bot.Send(photo)
+	return err
+}
 
 func main() {
 	token, err := GetToken()
@@ -36,33 +63,15 @@ func main() {
 		if update.Message.IsCommand() {
 			switch update.Message.Command() {
 			case "gradient":
-				userAvatar, err := GetUserAvatar(bot, update.Message.From.ID)
+				userId := update.Message.From.ID
+				chatId := update.Message.Chat.ID
+
+				err := handleGradient(bot, userId, chatId)
 				if err != nil {
-					log.Printf("[ERR] error geting user avatar: %v", err)
 					msg := tgbotapi.NewMessage(update.Message.Chat.ID, err.Error())
 					bot.Send(msg)
-					continue
 				}
-
-				gradient := CreateGradient(userAvatar.Bounds().Dx(), userAvatar.Bounds().Dy(), color.NRGBA{R: 0, G: 0, B: 255, A: 255}, color.NRGBA{R: 255, G: 0, B: 255, A: 255})
-
-				imgWithGradient := OverlayImage(userAvatar, gradient, 0.5)
-
-				var buf bytes.Buffer
-				err = imaging.Encode(&buf, imgWithGradient, imaging.PNG)
-				if err != nil {
-					log.Printf("[ERR] error saving imgWithGradient: %v", err)
-					msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Error due overlaying.")
-					bot.Send(msg)
-					continue
-				}
-
-				photo := tgbotapi.NewPhoto(update.Message.Chat.ID, tgbotapi.FileBytes{
-					Name:  "avatar_with_gradient.png",
-					Bytes: buf.Bytes(),
-				})
-				bot.Send(photo)
-
+				continue
 			default:
 				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Unknown command")
 				bot.Send(msg)
